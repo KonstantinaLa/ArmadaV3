43527ba4a7399943137f510c9e01c747b4d4c9b0
Kostas <Kwstaskor@hotmail.com>
2021-07-06 13:53:47 +0300

    Empire Create Started

--- Armada.WebApp/App_Start/BundleConfig.cs
+++ Armada.WebApp/App_Start/BundleConfig.cs
@@ -13,6 +13,9 @@ namespace Armada.WebApp
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/popper").Include(
+                "~/Scripts/umd/popper.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -23,6 +26,7 @@ namespace Armada.WebApp
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-solar.css",
+                      "~/Content/bootstrap-select.css",
                       "~/Content/site.css"));
         }
     }
--- Armada.WebApp/Scripts/MyJavaScript/Empire.js
+++ Armada.WebApp/Scripts/MyJavaScript/Empire.js
@@ -1,9 +1,8 @@
-﻿
-//Empire
+﻿//Create Table 
 function CreateEmpireTable() {
     $("#TabContent").html(`
                                           <h2 class="m-4 text-center">Empires</h2>
-                                            <table class="table table-hover table-bordered">
+                                            <table id="empireTable" class="table table-hover table-bordered">
                                                 <thead>
                                                     <tr>
                                                         <th>Photo</th>
@@ -17,20 +16,20 @@ function CreateEmpireTable() {
                                                 <tbody id="empiresBody">
                                             </tbody>
                                          </table>
-                                       <button id="create" onclick="" class="btn btn-primary m-1">Create New</button>
+                                       <button  onclick="ShowEmpireCreateModal()" class="btn btn-primary m-1">Create New</button>
                                         `);
 }
 
-$("#empireTablebtn").click(function () {
-    $.ajax({
-        type: "GET",
-        url: "/api/Empire",
-        data: "",
-        dataType: "json",
-        success: function (response) {
-            CreateEmpireTable();
-            response.forEach(function (empire) {
-                $("#empiresBody").append(`
+function CreateEmpireFullTable() {
+        $.ajax({
+            type: "GET",
+            url: "/api/Empire",
+            data: "",
+            dataType: "json",
+            success: function (response) {
+                CreateEmpireTable();
+                response.forEach(function (empire) {
+                    $("#empiresBody").append(`
                                                                  <tr id = "empire${empire.EmpireId}">
                                                                  <td>
                                                                      ${empire.Photo}
@@ -56,11 +55,13 @@ $("#empireTablebtn").click(function () {
                                                                  </td>
                                                              </tr>
                                                             `);
-            });
-        }
-    });
+                });
+            }
+        });
+    }
 
-});
+
+$("#empireTablebtn").click(()=>CreateEmpireFullTable());
 
 function ShowEmpireAdmirals(empire) {
     let template = "";
@@ -75,6 +76,128 @@ function ShowEmpireAdmirals(empire) {
     return template;
 }
 
+//Create Empire Section
+
+function ShowEmpireCreateModal() {
+    $(document).ready(function () {
+        $("#ArmadaModal").modal();
+    });
+
+    EmpireAdmiralsTemplate();
+    EmpireCreateModalBody();
+
+    $("#modalFooter").html('<button type="button" class="btn btn-secondary close" data-bs-dismiss="modal">Cancel</button>');
+
+    $(".close").click(function () {
+        $("#ArmadaModal").modal("hide");
+    });
+
+    $("#empireCreateForm").submit((e) => e.preventDefault());
+
+    CreateEmpire();
+
+    $("#empireCreateForm").submit(() => $("#ArmadaModal").modal("hide"));
+
+}
+
+function EmpireCreateModalBody() {
+    $("#modalBody").html(`
+                                    <div class="col-md-8 mx-auto text-center">
+                                         <form id="empireCreateForm">
+                                           <fieldset>
+                                               <legend>Register Emperor</legend>
+
+                                               <div class="form-group">
+                                                   <label class="col-form-label mt-4" for="Name">Name</label>
+                                                   <input type="text" class="form-control" placeholder="Name" id="Name" autocomplete="off" required minlength="2" >
+                                               </div>
+
+                                               <div class="form-group">
+                                                   <label class="col-form-label mt-4" for="Trait">Trait</label>
+                                                   <input type="text" class="form-control" placeholder="Trait" id="Trait" autocomplete="off" required minlength="2">
+                                               </div>
+
+                                                <div class="form-group">
+                                                   <label class="col-form-label mt-4" for="Systems">Controlled Systems</label>
+                                                   <input type="number" class="form-control" placeholder="Controlled Systems" id="Systems" autocomplete="off" required min ="1">
+                                               </div>
+
+                                               <div class="form-group">
+                                                   <label for="AdmiralSelect" class="form-label mt-4">Admirals</label>
+                                                   <select  id="AdmiralSelect" class=" form-control"  multiple name="Admirals">
+                                                    
+                                                   </select>
+                                               </div>
+
+
+                                               <div class="form-group">
+                                                   <label for="About" class="form-label mt-4">About</label>
+                                                   <textarea class="form-control" id="About" rows="3" required minlength="10"></textarea>
+                                               </div>
+                                               <div class="form-group">
+                                                   <label for="formFile" class="form-label mt-4">Photo</label>
+                                                   <input class="form-control" type="file" id="formFile">
+                                               </div>
+                                                  <input type="submit" class="btn btn-primary mt-3"  value="Register"/>
+                                              </fieldset>
+                                         </form>
+                                      </div>
+                                  `);
+}
+
+function EmpireAdmiralsTemplate() {
+    $.ajax({
+        type: "GET",
+        url: "/api/Admiral",
+        data: "",
+        dataType: "json",
+        success: function (response) {
+            
+            response.forEach(function (admiral) {
+                $("#AdmiralSelect").append(`
+                                                       <option id="${admiral.AdmiralId}">${admiral.Name}</option>
+                                               `);
+            });
+        }
+    });
+}
+
+function CreateEmpire() {
+
+   // var admirals = $.each($("#AdmiralSelect").children("option:selected"));
+
+    $("#empireCreateForm").submit(() => {
+        var empire = {
+            "Name": $("#Name").val(),
+            "Trait": $("#Trait").val(),
+            "ControlledSystems": $("#Systems").val(),
+            "Description": $("#About").val(),
+           // "Admirals": $("#AdmiralSelect").val(),
+            "Photo": null,
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "/api/Empire",
+            data: empire,
+            dataType: "json",
+            success: function (response) {
+                $("#successAlert").html(`
+                                       <div class="alert alert-dismissible alert-success">
+                                           <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                                           <strong>Well done!</strong> You successfully Created  ${response.Name} !!
+                                       </div>
+                                              `);
+                setTimeout(() => $("#successAlert").html('').fadeOut(4000), 4000);
+                $("#empireTable").html(' ');
+                CreateEmpireFullTable();
+            }
+        });
+    });
+}
+
+
+//Empire Info Section
 function ShowEmpireInfoModal(id) {
     $.ajax({
         type: "GET",
@@ -103,6 +226,8 @@ function ShowEmpireInfoModal(id) {
     });
 }
 
+
+//Empire Delete Section
 function ShowEmpireDeleteModal(id) {
     $(document).ready(function () {
         $("#ArmadaModal").modal();
